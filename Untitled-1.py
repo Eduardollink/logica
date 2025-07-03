0877b555fc32d6260b3e372942dfa218e8e81d95
EDUARDO LINK <05941667051@senacrs.edu.br>
2025-07-03 15:01:52 -0300

    Cálculo

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,5 +1,8 @@
-a = 8
-b = 2 
+a = int(input("Digite um valor para a:"))
+b = int(input("Digite um valor para b:"))
+
 soma = a + b 
+subt = a - b
 
 print(f"O resultado da soma é {soma}")
+print(f"O resultado da subtração é {subt}")
